5bcfb1b7868d1db26ce4da716fd32c385102b7c7
jpfreta <jpfreta@gmail.com>
2022-12-31 14:02:14 -0300

    teste outras  htmls (problema rota express)

--- src/configuracoes/rota.ts
+++ src/configuracoes/rota.ts
@@ -6,9 +6,4 @@ router.use(express.static(path.join('public')))
 router.get('/', (_req:Request,res:Response)=>{
     res.send('index')
 })
-router.get('/:sobre', (_req:Request,res:Response)=>{
-    res.send('Breve historia')
-})
-router.get('/:portifolio', (_req:Request,res:Response)=>{
-    res.send('Pequenos Projetos')
-})
+
