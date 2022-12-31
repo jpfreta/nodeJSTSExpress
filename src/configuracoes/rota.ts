5896968e8e6cdc57e2b1a65cd668238c39aa8ade
jpfreta <jpfreta@gmail.com>
2022-12-31 14:21:24 -0300

    report adiado estudos (rotas)

--- src/configuracoes/rota.ts
+++ src/configuracoes/rota.ts
@@ -1,9 +1,9 @@
 import  path  from 'path'
-import express, {Request, Response} from 'express'
+import express, { Request, Response} from 'express'
 export const router = express.Router()
 
 router.use(express.static(path.join('public')))
-router.get('/', (_req:Request,res:Response)=>{
-    res.send('index')
+router.get('/', (_request:Request, response:Response)=>{
+    response.send('index')
 })
 
