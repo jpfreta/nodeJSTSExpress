a48e61236963ba7e06b927ae909f7aee622a3fce
jpfreta <jpfreta@gmail.com>
2022-12-31 13:52:43 -0300

    ok

--- src/configuracoes/rota.ts
+++ src/configuracoes/rota.ts
@@ -6,3 +6,9 @@ router.use(express.static(path.join('public')))
 router.get('/', (_req:Request,res:Response)=>{
     res.send('index')
 })
+router.get('/:sobre', (_req:Request,res:Response)=>{
+    res.send('Breve historia')
+})
+router.get('/:portifolio', (_req:Request,res:Response)=>{
+    res.send('Pequenos Projetos')
+})
--- src/configuracoes/servidor.ts
+++ src/configuracoes/servidor.ts
@@ -3,6 +3,8 @@ import { router } from './rota'
 export const app =express()
 
 app.use('/', router)
+app.use('/:sobre', router)
+app.use('/:portifolio', router)
 
 app.listen(3000, ()=>{
     console.log('servidor online 3000')
